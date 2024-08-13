286d52ba45b8fb1d54cb5281c4fc951c735e8347
Anjali24-54 <155923819+Anjali24-54@users.noreply.github.com>
2024-08-13 15:41:30 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from the Backend Service!");
+  res.send("Hello from the Backend Service.This is backend service from a ASI Insurance Company!");
 });
 
 app.listen(port, () => {
